61a8920a11c363310c364289fa71425ecac075b0
Opilane TTHK <opilane@tthk.ee>
2020-10-05 14:52:06 +0300

    first commit

---
+++ WarderobePicken/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WarderobePicken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
